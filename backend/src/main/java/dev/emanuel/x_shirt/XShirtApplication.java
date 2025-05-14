3d79dd679c16e2eafeb8232d3a8e6cdd3df4e9f4
Emanuel <emanuelflavio1402@gmail.com>
2025-05-14 10:56:52 -0300

    first commit

---
+++ backend/src/main/java/dev/emanuel/x_shirt/XShirtApplication.java
@@ -0,0 +1,13 @@
+package dev.emanuel.x_shirt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XShirtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XShirtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/emanuel/x_shirt/XShirtApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.emanuel.x_shirt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XShirtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
