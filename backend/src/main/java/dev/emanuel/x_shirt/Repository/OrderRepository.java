dd251810b3bfc32c3b553aeef1818a56109833e3
Emanuel <emanuelflavio1402@gmail.com>
2025-05-16 19:46:12 -0300

    refactor: add @repository

--- backend/src/main/java/dev/emanuel/x_shirt/Repository/OrderRepository.java
+++ backend/src/main/java/dev/emanuel/x_shirt/Repository/OrderRepository.java
@@ -1,7 +1,10 @@
 package dev.emanuel.x_shirt.Repository;
 
 import dev.emanuel.x_shirt.Entity.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
