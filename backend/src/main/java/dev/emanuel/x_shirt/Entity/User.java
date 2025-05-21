fbac1ddb55daa1db7ab48540e95e634251bc8287
Emanuel <emanuelflavio1402@gmail.com>
2025-05-20 22:55:02 -0300

    feat: implements userdatails in user and create query for find user by email

--- backend/src/main/java/dev/emanuel/x_shirt/Entity/User.java
+++ backend/src/main/java/dev/emanuel/x_shirt/Entity/User.java
@@ -4,9 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,7 +18,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "tb_users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -60,6 +62,33 @@ public class User {
     private List<Favorites> favorites;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
 
+    @Override
+    public String getUsername() {
+        return "";
+    }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- backend/src/main/java/dev/emanuel/x_shirt/Repository/UserRepository.java
+++ backend/src/main/java/dev/emanuel/x_shirt/Repository/UserRepository.java
@@ -2,8 +2,14 @@ package dev.emanuel.x_shirt.Repository;
 
 import dev.emanuel.x_shirt.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<UserDetails> findUserByEmail(String email);
 }
