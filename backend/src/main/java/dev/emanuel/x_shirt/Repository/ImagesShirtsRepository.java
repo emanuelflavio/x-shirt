3a5c6877be392cf3b0d2f6b25c14e2feaae79365
Emanuel <emanuelflavio1402@gmail.com>
2025-05-26 23:08:27 -0300

    feat: create query to find by variations in images

--- backend/src/main/java/dev/emanuel/x_shirt/Repository/ImagesShirtsRepository.java
+++ backend/src/main/java/dev/emanuel/x_shirt/Repository/ImagesShirtsRepository.java
@@ -1,9 +1,16 @@
 package dev.emanuel.x_shirt.Repository;
 
 import dev.emanuel.x_shirt.Entity.ImagesShirts;
+import dev.emanuel.x_shirt.Entity.Shirt;
+import dev.emanuel.x_shirt.Entity.Variations;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImagesShirtsRepository extends JpaRepository<ImagesShirts, Long> {
+
+    List<ImagesShirts> findByVariations(Variations variationsId);
+
 }
