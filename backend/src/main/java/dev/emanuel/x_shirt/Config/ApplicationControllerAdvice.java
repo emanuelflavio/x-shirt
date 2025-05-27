f008e6198e9d9ab1990b3bd5ee4d1f91ac3cfd8b
Emanuel <emanuelflavio1402@gmail.com>
2025-05-26 23:14:58 -0300

    feat: create controlleradvice to exceptions

--- backend/src/main/java/dev/emanuel/x_shirt/Config/ApplicationControllerAdvice.java
+++ backend/src/main/java/dev/emanuel/x_shirt/Config/ApplicationControllerAdvice.java
@@ -1,13 +1,18 @@
 package dev.emanuel.x_shirt.Config;
 
 
+import dev.emanuel.x_shirt.exception.FileNotFoundException;
+import dev.emanuel.x_shirt.exception.FileStorageException;
 import dev.emanuel.x_shirt.exception.UsernameOrPasswordInvalidException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -21,12 +26,24 @@ public class ApplicationControllerAdvice {
         return ex.getMessage();
     }
 
+    @ExceptionHandler(FileNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleFileNotFoundException(UsernameOrPasswordInvalidException ex) {
+        return ex.getMessage();
+    }
+
+    @ExceptionHandler(FileStorageException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleNotFoundException(FileStorageException ex) {
+        return ex.getMessage();
+    }
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex){
+    public Map<String, String> handleArgumentNotValidException(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
-            errors.put(((FieldError)error).getField(), error.getDefaultMessage());
+            errors.put(((FieldError) error).getField(), error.getDefaultMessage());
         });
         return errors;
     }
