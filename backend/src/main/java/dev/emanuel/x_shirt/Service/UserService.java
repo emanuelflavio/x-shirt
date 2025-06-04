841e12fc04e8efeb1cb34c342ce6e03fa5e6b2a2
Emanuel <emanuelflavio1402@gmail.com>
2025-06-04 15:02:55 -0300

    feat: create find by id, find all, delete by id and update to user

--- backend/src/main/java/dev/emanuel/x_shirt/Service/UserService.java
+++ backend/src/main/java/dev/emanuel/x_shirt/Service/UserService.java
@@ -6,6 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
@@ -19,4 +22,29 @@ public class UserService {
         user.setPassword(passwordEncoder.encode(password));
         return userRepository.save(user);
     }
+
+    public Optional<User> findById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public void deleteById(Long id){
+        userRepository.deleteById(id);
+    }
+
+    public Optional<User> update(Long id, User user){
+        Optional<User> userOptional = userRepository.findById(id);
+        if(userOptional.isPresent()){
+            User userNew = userOptional.get();
+            userNew.setName(userNew.getName());
+            userNew.setPassword(passwordEncoder.encode(userNew.getPassword()));
+            userNew.setEmail(user.getEmail());
+            return Optional.of(userNew);
+        }
+        return Optional.empty();
+    }
+
 }
